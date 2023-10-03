8d8e999391784720c179584d7b93e17636cf7fa4
Vitor Marcus Dutra <ntw1942@gmail.com>
2023-10-03 08:51:57 -0300

    fix: adding mv to new certificate

--- script.sh
+++ script.sh
@@ -43,6 +43,8 @@ if [ "$TYPE" = "proxmox" ]; then
     ssh $USER@$CONNECTION -t 'mv /etc/pve/'${NODE}'/pve-ssl.pem /etc/pve/'${NODE}'/pve-ssl.old'
     ssh $USER@$CONNECTION -t 'mv /etc/pve/'${NODE}'/pve-key.pem /etc/pve/'${NODE}'/pve-key.old'
     scp -r $PATH_FROM $USER@$CONNECTION:$PATH_TO
+    ssh $USER@$CONNECTION -t 'mv '${PATH_TO}'/fullchain.pem /etc/pve/'${NODE}'/pve-ssl.pem'
+    ssh $USER@$CONNECTION -t 'mv '${PATH_TO}'/privkey.pem /etc/pve/'${NODE}'/pve-key.pem'
     ssh $USER@$CONNECTION -t 'systemctl restart pveproxy'
 fi
 
